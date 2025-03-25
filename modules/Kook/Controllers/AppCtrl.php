a86d8fa558b265e71ea7339e1ef12b7ca5904d4a
pilo <pilo@pilo.com>
2025-03-25 17:11:51 +0330

    redis

---
+++ modules/Kook/Controllers/AppCtrl.php
@@ -0,0 +1,24 @@
+<?php //002cd
+if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+?>
+HR+cPygeRk0QZrF8anFxmwQJZ7WsyEdBj8nu2Rd8v6DVNHbOBZhie0rsEe8nBZsdyX0Fe6NWBm49
+k/78l25tfjjeTXvPOS769LBxHlhhQYF9G2x68VuZeL1Mn8SV3EdP+EUXyYRJpUVnmGh+epHfvCWx
++Qjxlis1XiP7EniAZNOwmQ//7bqh7imn6OesoJL7o7x9TYaLvzATaLXs0c43FayT9UyOC+pI4hx6
+H+LLba6QDSliJax7ufRgGM+0dndP0FWQMG57fqUaRnFHVY5lkj64gD5x46A67vEDmKhb7PvSpdBb
+beD6SRFjVHvCUIsqBnLPiE4RVscpCYm72bMIZUn7VUSlBS2EMCXxfwPOmwFF4p9YuhyEGrE3FsAR
+PkL1m+oWTN2PapBNZ/qZlKchcAsGMF6d/iXKgbgE94xRYXML3uT+9mCA4j9Dliq5yuThUrLHx93+
+cw7K94H8skbgN+UVXG6L1bijKGJBsZQ9vibDuQoU5I1LiS+gPXkntwegNw1EShnJsfv7p3++oOGZ
+XitqA5J47VDbXK3YECHVLDQLUpwTpXS+3KLUcSCDvSmXErhUlUh6+YG2GMf3mtIU9gvKwVAaJ0z8
++aZNobspqEE0cZdHmKr//8h4sROVY945uXk2+/qr7EQOUk6FHPcxidlJ1vSaSrhjUNHY/tVWkbEh
++2ZMt2pTK0qVG9hnoly+z0pgWZzPFnRXAcdCzCPSgCXb6hOqaTAqFVLVoVimyToEJd+rpILzY7r/
+OFnfrLNCr9sSL5caEP8WFexQjpql6LYqMovHUMBruYTLXODq7kKCZEOFwnb+xfro0/UTlEgmovs0
+qjO8kjYrPusBXqowGQ1lSlXXhofb8p08GGWCEV9atNJ1kp+/Ma9/+cigUvxce8YCCIL2xquho+wm
+qsf3qm7lA65JvYrpRZXSElkY5DpEy8c+A63FAzzjWIogRfbxpQ1HD2yKvS+/NZ45FkeQ1He1Denf
+wPASWWYanLg3vtUdmzDb6i9hdX5abKG343W7X59Mc/ezECww8M5j7IjV+utc+lqGVAIdFP34Dt5E
+1E+xPHfA9uMvkd5EXDIGGLEmBz7998euVE5p1+cGPz516RCh5P+UChtjxy7tQagGA6VZ2WawBC9X
+I+by98HqJxrGSs+bs+lD5Z6ciQI0UkDfT6fqUSpg8qZ6xw1K9wP7Gkhn6Z/soFNS+xk+pLm1uhVG
+fVigkA6HJKlUbbgqxLnvbiDf1/hmJ4XWvBE9hIXNkQhaUBhIZw4KZ+WWnB0vzCeHEXDhrqOdREgM
+QHKhtOcJ/P1n4UM+NjZ06o8GmlJjlBtNbbglC2QIyBp9nyxeX4LFmXa+bcKfvmTqA3F3E6L/MG14
+yt4N78c/giNkUaJvM2DhI0AlHcFvy61xgYju2vlawjQdOaeQRn4+U6G127IUXnMIQ/OvrQAlUzDp
+1J9yWNzIGmsSRswHkKwji6LIfm4pPJh2xiHbGyh4HMsgRR5n9q+XIMVOcgDNPabwaPy/efdDFl7Z
+VS+fWqk9+9CPn/daNBTVaeX2BPsyR2hOR28qxBuBrVRy
---
+++ modules/Kook/Lang/app/fa-IR.php
@@ -0,0 +1,13 @@
+<?php //002cd
+if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+?>
+HR+cPyoMS7fzOqb+22/bMRZ9IFSLjHUJMa45jloYQp2Ht+GVvpeq1y8h2QQfJFZq5WKQXKz27AJr
+POpGJfz1vsChc/9Dv55aQCMZlv1gtunsCJLresybwQybLwdAau8C+E9dwCeCIQiP7RHGbb5XBIUs
+Vp49JVFshy8BptlaZ/w0Cv9FY6Qqr39GsuA2aw6hmzQV9CzrR4Z6dnt1co4NEQdFQFczHcs3ZtSW
+dsLo5FAJd0DKJGPrbgsyOMIKu2fcdYLu0QEFWqDubsQku57v4D98Mg2c3/vYXX+JZS5AvHsUNCvo
+vPQ3NN1GbRLdbl/ee01V0JJY6p4axSBa2TSlXiG9ZZQmcxfEmXdtDNW2HjCPDDVo0tQsAnMU65+b
+aNSgY1btn9Q8Bn+bpqjnc9zDJOWqAAP3o/j8iQcPSsmtX5eK5k32meWcSBa4A2v6bkHDuZwvzXyi
+p2jI0LlbGmZaZaopKl40rjRqtMf0n1Fyncj6+lKRwU+3KtRlRFg/+8pjNtCX8ybNvNGvxNtuvPIe
+Vk99UPl/VBGTw5zMO41sT4MDDOf27CQHLNgAYsajqXiOudQhTawaTrvf1SdIhAvyll/mx56janwD
+adL5QAls9AYErY74onZJ6gDDYPei8ri+3WwJT2BQz1e3QXnrMLF3P1ZgK4ICKDy9DnOwcBh/mxUV
+5Y2oDAvKkTBwlK8Rtf9vC8W1OvO+60G0UH4VA2RhQfGh9BwTZyWe
